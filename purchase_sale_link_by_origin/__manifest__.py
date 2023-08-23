174d78251657ec46b41a4ff3b657a2c58299a5ac
Phuc <hoangphuc2984@gmail.com>
2023-08-23 11:21:36 +0700

    [16.0][ADD] purchase_sale_link_by_origin

---
+++ purchase_sale_link_by_origin/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+{
+    'name': 'AFS Sale & Purchase',
+    'version': '16.0.1.0.0',
+    'author': 'Camptocamp',
+    'license': 'AGPL-3',
+    'category': 'Purchase',
+    'website': 'https://github.com/OCA/purchase-workflow',
+    'depends': [
+        'sale_purchase_stock',
+    ],
+    'data': [],
+    'installable': True,
+}
---
+++ purchase_sale_link_by_origin/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+* When users cancel a Purchase Order/ Sale Order, the smart button Purchase on module Sales and Sale on module Purchase don't disappears.
